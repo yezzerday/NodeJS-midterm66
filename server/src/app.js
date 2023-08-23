4d0bf4953e4614c275dd41c1f77705d84ebd4488
yezzerday <jaruda.17502@gmail.com>
2023-08-23 12:47:37 +0700

    server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,24 @@
-console.log('hello')
+let express = require('express')
+const app = express()
+app.get('/status',function(req,res){
+    res.send('hello js')
+})
+
+app.get('/hello/:person',function(req,res){
+    console.log('hello - '+ req.params.person)
+    res.send('say hello with '+ req.params.person)
+})
+
+app.get('/user/:userID',function(req,res){
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+app.get('/users',function (req,res){
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
+})
+
+let port = 8081
+
+app.listen(port,function(){
+    console.log('server running on '+port)
+})
